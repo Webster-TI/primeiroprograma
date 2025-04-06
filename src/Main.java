a39538dedb935639d0e1d908503d7647e16297ba
Webster Spacacheri <ti.spacacher@gmail.com>
2025-04-06 20:08:09 -0300

    Como comentar código utilizando //, utilização da classe Math e o método Math.round para arredondar um número para o inteiro mais próximo e como utilizar a String para formatar textos utilizando """ 3 aspas duplas.

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,21 @@ public class Main {
 
         int anoDeLancamento = 2022;
         System.out.println("Ano de Lançamento: " + anoDeLancamento);
-        boolean incluidoNoPlano = true;
+        /*boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
         System.out.println("Incluído no Plano? " + incluidoNoPlano);
-        System.out.println("Nota fo Filme: " + notaDoFilme);
+        System.out.println("Nota fo Filme: " + notaDoFilme);*/
+        double media = (9.8 + 6. + 8.0) / 3;
+        // Arredondar para 1 casa decimal
+        media = Math.round(media * 10.0) / 10.0;
+        System.out.println(media);
+        String sinopse;
+        sinopse = """
+                Filme Top Gun
+                Filme de aventura com galã dos anos 80
+                Muito bom!
+                Ano de Lançamento
+                """ + anoDeLancamento;
+        System.out.println(sinopse);
     }
 }
